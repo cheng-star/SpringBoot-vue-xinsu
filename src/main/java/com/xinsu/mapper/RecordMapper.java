d8743b9264f24889d806e5e4a4ffb282650e49b2
cheng-star <1522542949@qq.com>
2020-07-15 21:59:18 +0800

    增加删除交易记录功能

--- src/main/java/com/xinsu/mapper/RecordMapper.java
+++ src/main/java/com/xinsu/mapper/RecordMapper.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Repository
 public interface RecordMapper {
 
-    List<Record> queryRecordByID(int ID);
+    List<Record> queryRecordByID(int serial);
     int addRecord(Record record);
+    int deleteRecords(int serial);
 }
