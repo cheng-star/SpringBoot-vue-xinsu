61ea213e8f8b21006073f9af07dd9d2c81d0c18e
cheng-star <1522542949@qq.com>
2020-07-16 17:27:43 +0800

    用户接口

---
+++ src/main/java/com/xinsu/controller/StuController.java
@@ -0,0 +1,107 @@
+package com.xinsu.controller;
+
+import com.xinsu.pojo.Record;
+import com.xinsu.pojo.Result;
+import com.xinsu.pojo.Stu;
+import com.xinsu.service.RecordServiceImpl;
+import com.xinsu.service.StuService;
+import com.xinsu.service.StuServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yeyike
+ * @date 2020/7/15 - 19:52
+ */
+@RestController
+@CrossOrigin
+public class StuController {
+    @Autowired
+    StuServiceImpl stuService;
+    @Autowired
+    RecordServiceImpl recordService;
+    @Autowired
+    JavaMailSenderImpl mailSender;
+
+    @RequestMapping("/consumption")
+    public Result consumption(Stu stu){
+        Stu stu1 = stuService.queryStuByID(stu.getID());
+        int price = stu1.getBalance() - stu.getBalance();
+        int result1 = stuService.updateStu(stu);
+        if (result1 > 0) {
+            Record record = new Record("消费了"+price+"元",stu.getID());
+            int result2 = recordService.addRecord(record);
+            if (result2 > 0){
+                SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+                mailMessage.setSubject("新素会员卡消费通知");
+                mailMessage.setText("今天您已消费"+price+"元");
+                mailMessage.setTo(stu.getMail());
+                mailMessage.setFrom("ad2248@foxmail.com");
+                mailSender.send(mailMessage);
+                return new Result("true");
+            }else {
+                return new Result("false");
+            }
+
+        }else {
+            return new Result("false");
+        }
+
+    }
+
+    @RequestMapping("/recharge")
+    public Result recharge(Stu stu){
+        Stu stu1 = stuService.queryStuByID(stu.getID());
+        int price = stu.getBalance() - stu1.getBalance();
+        int result1 = stuService.updateStu(stu);
+        if (result1 > 0) {
+            Record record = new Record("充值了"+String.valueOf(price)+"元",stu.getID());
+            int result2 = recordService.addRecord(record);
+            if (result2 > 0){
+                SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+                mailMessage.setSubject("新素会员卡消费通知");
+                mailMessage.setText("今天您已充值"+price+"元");
+                mailMessage.setTo(stu.getMail());
+                mailMessage.setFrom("ad2248@foxmail.com");
+                mailSender.send(mailMessage);
+                return new Result("true");
+            }else {
+                return new Result("false");
+            }
+
+        }else {
+            return new Result("false");
+        }
+
+    }
+
+    @RequestMapping("/deleteStu")
+    public Result deleteStu(int ID){
+        Stu stu = stuService.queryStuByID(ID);
+        int result1 = stuService.deleteStu(ID);
+        if (result1 > 0){
+            int result2 = recordService.deleteRecords(ID);
+            if (result2 > 0){
+                SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+                mailMessage.setSubject("新素会员卡注销通知");
+                mailMessage.setText("今天您已将新素会员卡注销");
+                mailMessage.setTo(stu.getMail());
+                mailMessage.setFrom("ad2248@foxmail.com");
+                mailSender.send(mailMessage);
+                return new Result("true");
+            }else {
+                return new Result("false");
+            }
+        }else {
+            return new Result("false");
+        }
+    }
+
+}
