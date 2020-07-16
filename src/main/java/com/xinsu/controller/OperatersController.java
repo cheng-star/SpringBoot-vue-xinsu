10a49ea2e953fe35764ce42de1f172673a126a95
cheng-star <1522542949@qq.com>
2020-07-16 21:35:13 +0800

    登录功能

--- src/main/java/com/xinsu/controller/OperatersController.java
+++ src/main/java/com/xinsu/controller/OperatersController.java
@@ -1,14 +1,19 @@
 package com.xinsu.controller;
 
 import com.xinsu.pojo.Operaters;
+import com.xinsu.pojo.Result;
 import com.xinsu.service.OPeratersServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.time.Duration;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author yeyike
@@ -21,8 +26,23 @@ public class OperatersController {
     @Autowired
     OPeratersServiceImpl oPeratersService;
 
+    @Resource
+    private RedisTemplate<String,Object> redisTemplate;
+
     @RequestMapping("/getOperaters")
-    public List<Operaters> getOperaters(){
-        return oPeratersService.queryOperatersList();
+    public Result getOperaters(String name, String passwd){
+
+        Operaters operaters =  oPeratersService.queryOperatersByName(name);
+        if (operaters != null){
+            if (operaters.getPasswd().equals(passwd)){
+                String token = UUID.randomUUID()+"";
+                redisTemplate.opsForValue().set(token,operaters, Duration.ofMinutes(30L));
+                return new Result(token,"true");
+            }else {
+                return new Result("false");
+            }
+        }else {
+            return new Result("false");
+        }
     }
 }
