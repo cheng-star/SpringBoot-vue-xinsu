6d015a568e256c33ba2059043d4f651826defb12
cheng-star <1522542949@qq.com>
2020-07-18 16:52:02 +0800

    测试redis数据库

--- src/test/java/com/xinsu/TeamApplicationTests.java
+++ src/test/java/com/xinsu/TeamApplicationTests.java
@@ -5,21 +5,50 @@ import com.xinsu.pojo.Operaters;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest
 class TeamApplicationTests {
     @Autowired
     private OperatersMapper operatersMapper;
+    @Autowired
+    JavaMailSenderImpl mailSender;
+
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
 
     @Test
     void contextLoads() {
 
-        List<Operaters> operaters = operatersMapper.queryOperatersList();
-        for (Operaters operaters1:operaters){
-            System.out.println(operaters1);
+        Operaters operaters =  operatersMapper.queryOperatersByName("root");
+        System.out.println(operaters);
+        if (operaters.getPasswd().equals("abc12138de")){
+            System.out.println("true");
         }
+    }
+    @Test
+    public void mailText(){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setSubject("新素会员卡消费通知");
+        mailMessage.setText("今天您已消费");
+        mailMessage.setTo("1522542949@qq.com");
+        mailMessage.setFrom("ad2248@foxmail.com");
+        mailSender.send(mailMessage);
+    }
+    @Test
+    public void testRedis(){
+        Operaters operaters = new Operaters();
+        String token = UUID.randomUUID()+"";
+        System.out.println(token);
+        redisTemplate.opsForValue().set(token,operaters, Duration.ofMinutes(30L));
+        System.out.println(redisTemplate.opsForValue().get(token));
     }
 
 }
