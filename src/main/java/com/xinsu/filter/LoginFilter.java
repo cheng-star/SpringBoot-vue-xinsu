ee1e178fe0220bf1aa7dcc23c2aa14e1e5ffc901
cheng-star <1522542949@qq.com>
2020-07-20 19:23:47 +0800

    属性修改

--- src/main/java/com/xinsu/filter/LoginFilter.java
+++ src/main/java/com/xinsu/filter/LoginFilter.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -23,6 +24,7 @@ import java.util.concurrent.TimeUnit;
  * @date 2020/7/17 - 10:49
  */
 @WebFilter(urlPatterns = {"/view/*"})
+@CrossOrigin
 public class LoginFilter implements Filter {
 
     @Autowired
@@ -35,25 +37,25 @@ public class LoginFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         //Filter过滤器跨域处理
-        String origin = request.getHeader("Origin");
+//        String origin = request.getHeader("Authorization");
 
-        response.setHeader("Access-Control-Allow-Origin",origin);
-        response.setHeader("Access-Control-Allow-Credentials","true");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,X-App-Id, X-Token");
-        response.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Origin",origin);
+//        response.setHeader("Access-Control-Allow-Credentials","true");
+//        response.setHeader("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,X-App-Id, X-Token");
+//        response.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//        response.setHeader("Access-Control-Max-Age", "3600");
 
-        // 如果是OPTIONS则结束请求
-        if (HttpMethod.OPTIONS.toString().equals(request.getMethod())) {
-            response.setStatus(HttpStatus.NO_CONTENT.value());
-            String string = JSONObject.toJSONString(new Result("false"));
-            response.setContentType("json/text;charset=utf-8");
-            PrintWriter out = response.getWriter();
-            out.write(string);
-        }
+//        // 如果是OPTIONS则结束请求
+//        if (HttpMethod.OPTIONS.toString().equals(request.getMethod())) {
+//            response.setStatus(HttpStatus.NO_CONTENT.value());
+//            String string = JSONObject.toJSONString(new Result("false"));
+//            response.setContentType("json/text;charset=utf-8");
+//            PrintWriter out = response.getWriter();
+//            out.write(string);
+//        }
 
         //获取Headers中的参数
-        String token = request.getHeader("token");
+        String token = request.getHeader("Authorization");
         token = token==null ? "" : token;
         //查询Redis中是否存在
         Long expire = redisTemplate.getExpire(token);
